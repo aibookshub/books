0a0e598592491eb70167c799a6f7aa1422cb62d3
aiaccfin <aiaccfin@gmail.com>
2025-02-03 09:22:41 -0500

    update

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -7,7 +7,7 @@ const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => (
     <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name="AI-Powered BooksHub" component={StackNavigator} />
+        <Drawer.Screen name="📚AI-Powered BooksHub" component={StackNavigator} />
     </Drawer.Navigator>
 );
 
